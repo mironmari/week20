29f3038a92932aea2aa20b74a7093fac50d67edb
[mironmari] <[mironrnko.mv@mail.ru>
2023-07-04 10:59:55 +0100

    second commit

--- week20.2КалькуляторАвто/assets/index.js
+++ week20.2КалькуляторАвто/assets/index.js
@@ -1,132 +1,59 @@
-/*const form  = document.forms.my_form;
-console.log(1, form);
+let my_form = document.forms.my_form;
 
-const email = document.getElementById('email');
-console.log(2, email);
-const emailError = document.getElementById('email__error');
-const age = document.getElementById('age');
-console.log(4, age);
-const ageError = document.getElementById('age__error');
-const name1 = document.getElementById('name');
-console.log(7, name1);
-const name1Error = document.getElementById('name__error');
-console.log(11, name1);
+
+function calculatePrice() {
+
+if (my_form.brend.onchange = function() {
+  console.log(my_form.brend.value)});
+  return false;
+}
 
 
 
-email.addEventListener('input', function (event) {
-  // Каждый раз, когда пользователь что-то вводит,
-  // мы проверяем, являются ли поля формы валидными
+my_form.model.onchange = function() {
+  console.log(my_form.model.value);
+}
 
-  if (email.validity.valid) {
-    // Если на момент валидации какое-то сообщение об ошибке уже отображается,
-    // если поле валидно, удаляем сообщение
-    emailError.textContent = ''; // Сбросить содержимое сообщения
-    emailError.className = 'error'; // Сбросить визуальное состояние сообщения
-  } else {
-    // Если поле не валидно, показываем правильную ошибку
-    showErrorEmail();
-  }
-});
+my_form.fuelType[0].onchange = function() {
+  console.log(my_form.fuelType[0].value);
+}
+my_form.fuelType[1].onchange = function() {
+  console.log(my_form.fuelType[1].value);
+}
+my_form.fuelType[2].onchange = function() {
+  console.log(my_form.fuelType[2].value);
+}
+my_form.fuelType[3].onchange = function() {
+  console.log(my_form.fuelType[3].value);
+}
 
-age.addEventListener('input', function (event) {
-  // Каждый раз, когда пользователь что-то вводит,
-  // мы проверяем, являются ли поля формы валидными
+my_form.engineCapacity.onchange = function() {
+  console.log(my_form.engineCapacity.value);
+}
 
-  if (age.validity.valid) {
-    // Если на момент валидации какое-то сообщение об ошибке уже отображается,
-    // если поле валидно, удаляем сообщение
-    ageError.textContent = ''; // Сбросить содержимое сообщения
-    ageError.className = 'error'; // Сбросить визуальное состояние сообщения
-  } else {
-    // Если поле не валидно, показываем правильную ошибку
-    showErrorAge();
-  }
-});
+my_form.conditions[0].onchange = function() {
+  console.log(my_form.conditions[0].value);
+}
+my_form.conditions[1].onchange = function() {
+  console.log(my_form.conditions[1].value);
+}
 
-name1.addEventListener('input', function (event) {
-  // Каждый раз, когда пользователь что-то вводит,
-  // мы проверяем, являются ли поля формы валидными
+my_form.owner[0].onchange = function() {
+  console.log(my_form.owner[0].value);
+}
+my_form.owner[1].onchange = function() {
+  console.log(my_form.owner[1].value);
+}
 
-  if (name1.validity.valid) {
-    // Если на момент валидации какое-то сообщение об ошибке уже отображается,
-    // если поле валидно, удаляем сообщение
-    name1Error.textContent = ''; // Сбросить содержимое сообщения
-    name1Error.className = 'error'; // Сбросить визуальное состояние сообщения
-  } else {
-    // Если поле не валидно, показываем правильную ошибку
-    showErrorName1();
-  }
-});
+my_form.payment[0].onchange = function() {
+  console.log(my_form.payment[0].value);
+}
+my_form.payment[1].onchange = function() {
+  console.log(my_form.payment[1].value);
+}
+my_form.payment[2].onchange = function() {
+  console.log(my_form.payment[2].value);
+}
 
-form.addEventListener('submit', function (event) {
-  console.log(3, email.value);
-  console.log(5, age.value);
-  console.log(6, name1.value);
-  // Если поле email валдно, позволяем форме отправляться
-
-  if(!email.validity.valid) {
-    // Если поле email не валидно, отображаем соответствующее сообщение об ошибке
-    showErrorEmail();
-    // Затем предотвращаем стандартное событие отправки формы
-    event.preventDefault();
-  }
-  if(!age.validity.valid) {
-    // Если поле email не валидно, отображаем соответствующее сообщение об ошибке
-    showErrorAge();
-    // Затем предотвращаем стандартное событие отправки формы
-    event.preventDefault();
-
-  }
-  if(!name1.validity.valid) {
-    // Если поле email не валидно, отображаем соответствующее сообщение об ошибке
-    showErrorName1();
-    // Затем предотвращаем стандартное событие отправки формы
-    event.preventDefault();
-  }
-});
-
-function showErrorEmail() {
-  if(email.validity.valueMissing) {
-    // Если поле пустое,
-    // отображаем следующее сообщение об ошибке
-    emailError.textContent = 'You need to enter an e-mail address.';
-  } else if(email.validity.typeMismatch) {
-    // Если поле содержит не email-адрес,
-    // отображаем следующее сообщение об ошибке
-    emailError.textContent = 'Entered value needs to be an e-mail address.';
-  } else if(email.validity.tooShort) {
-    // Если содержимое слишком короткое,
-    // отображаем следующее сообщение об ошибке
-    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
-
-    emailError.className = 'error active';
-  }}
-
-function showErrorAge() {
-  if(age.validity.valueMissing) {
-    // Если поле пустое,
-    // отображаем следующее сообщение об ошибке
-    ageError.textContent = 'You need to enter your age';
-
-    ageError.className = 'error active';
-  }}
-
-function showErrorName1() {
-  if(name1.validity.valueMissing) {
-    // Если поле пустое,
-    // отображаем следующее сообщение об ошибке
-    name1Error.textContent = 'You need to enter Name';
-  } else if(name1.validity.patternMismatch) {
-    // Если поле содержит не email-адрес,
-    // отображаем следующее сообщение об ошибке
-    name1Error.textContent = 'It is allowed to use only spaces and Russian letters';
-  } else if(name1.validity.tooShort) {
-    // Если содержимое слишком короткое,
-    // отображаем следующее сообщение об ошибке
-    name1Error.textContent = `name should be at least ${name1.minLength} characters; you entered ${name1.value.length }.`;}
-    name1Error.className = 'error active';
-  }
-
-*/
-
+calculatePrice()
+console.log(1, calculatePrice);
